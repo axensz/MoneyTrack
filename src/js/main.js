d593a506ce1bf9f7f9a33662325addf25ec78283
Mkclaren <mkclaren.tobar@upb.edu.co>
2025-02-21 16:13:08 -0500

    V1.7 - Estilos Recuperar contaseña

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 //Funcion para deshabilitar el boton de recuperar contraseña
 function deshabilitarBoton() {
-    let boton = document.getElementById("miBoton");
-    let mensaje = document.getElementById("mensaje");
+    let boton = document.querySelector(".BotonVerificar");
+    let mensaje = document.querySelector(".Contrasena");
 
     boton.disabled = true; // Deshabilita el botón
-    mensaje.textContent = "El botón ha sido deshabilitado."; // Muestra el mensaje
+    mensaje.textContent = "Tu contraseña es: 123456"; // Muestra la contraseña
 }
+
 
